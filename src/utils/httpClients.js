4a76bbd46018c06018d40e053c6e4b2b6e3911e1
herinyhert <herinyhert@gmail.com>
2023-02-24 13:19:48 -0300

    previous comment is corrected

--- src/utils/httpClients.js
+++ src/utils/httpClients.js
@@ -1,10 +1,10 @@
 const API = "https://api.themoviedb.org/3";
-
+const {REACT_APP_URL} = process.env
 export function get(path){
+  // console.log("estamos aqui", process.env.REACT_APP_URL)
     return fetch(API + path, {
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTRiYzBjODhjZTE0ODM0OGZiNzg5NDE4YzE0NjQxNSIsInN1YiI6IjYzZjdkODI0MWYzMzE5MDA4YzUwMzIzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i-XO3XOt7viJUdZ9ukFHGxZifEYGe2O0Vy2gt5jeccQ",
+        Authorization: REACT_APP_URL,
         "Content-Type": "application/json;charset=utf-8",
       },
     })
